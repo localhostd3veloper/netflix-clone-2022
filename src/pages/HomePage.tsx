e89bc36952e548d732085213b4601710df4dad15
Gautam Anand <gautamanand022@gmail.com>
2022-08-07 21:06:51 +0530

    add: Mute Icon

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,16 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { InfoIcon } from "../data/svgs/info-icon";
+import { MutedIcon } from "../data/svgs/muted-icon";
 import { PlayIcon } from "../data/svgs/play-icon";
+import { SoundIcon } from "../data/svgs/sound-icon";
 import { TopTen } from "../data/svgs/top-ten";
 const HomePage: FC = () => {
+	const [isSoundOn, setIsSoundOn] = useState(true);
 	return (
 		<div className="shadow-2xl">
 			<div id="video-item">
 				<video
-					muted={window.location.hostname === "localhost" ? true : false}
+					muted={!isSoundOn}
 					autoPlay
 					loop
 					className="h-screen w-screen object-cover"
@@ -15,7 +18,7 @@ const HomePage: FC = () => {
 					<source src={require("../data/video/trailer.mp4")} type="video/mp4" />
 					Your browser does not support HTML5 video.
 				</video>
-				<div className="absolute w-4/12 left-32 bottom-[15vh] flex flex-col gap-4">
+				<div className="absolute  left-16 right-0 bottom-[15vh] flex flex-col gap-4">
 					<div className="flex items-center gap-2 text-xs tracking-widest">
 						<img
 							src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Netflix_2015_N_logo.svg/1200px-Netflix_2015_N_logo.svg.png"
@@ -35,21 +38,32 @@ const HomePage: FC = () => {
 							#4 in TV Shows Today
 						</span>
 					</div>
-					<div className="text-xl font-light">
+					<div className="text-xl w-2/6 font-light">
 						When a young boy vanishes, a small town uncovers a mystery involving
 						secret experiments, terrifying supernatural forces and one strange
 						little girl.
 					</div>
 					<div>
-						<div className="flex gap-4 text-lg items-center">
-							<button className="bg-white flex text-black rounded-md px-7 items-center py-2">
-								<PlayIcon />
-								PLAY
-							</button>
-							<button className="bg-gray-800/50 flex items-center rounded-md px-5 py-2">
-								<InfoIcon />
-								More Info
-							</button>
+						<div className="flex gap-4 text-lg items-center w-full justify-between">
+							<div className="flex gap-5">
+								<button className="bg-white flex text-black rounded-md px-7 items-center py-2">
+									<PlayIcon />
+									PLAY
+								</button>
+								<button className="bg-gray-800/50 flex items-center rounded-md px-5 py-2">
+									<InfoIcon />
+									More Info
+								</button>
+							</div>
+							<div
+								className="hover:opacity-70  rounded-full ml-auto border p-2"
+								onClick={() => setIsSoundOn(!isSoundOn)}
+							>
+								{isSoundOn ? <SoundIcon /> : <MutedIcon />}
+							</div>
+							<div className="bg-gray-800/40 border-l-2 w-1/12 tracking-wider text-center">
+								U/A 13+
+							</div>
 						</div>
 					</div>
 				</div>
