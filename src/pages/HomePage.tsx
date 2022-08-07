9b7d007b7571de7b18895bdc59232b2d643277d3
Gautam Anand <gautamanand022@gmail.com>
2022-08-07 11:06:16 +0530

    feat: change trailer

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,17 @@
 import { FC } from "react";
-import Video from "./trailer.mp4";
 const HomePage: FC = () => {
 	return (
 		<div className="">
 			<div className="h-screen">
 				<video autoPlay loop className="h-screen w-screen fixed object-cover">
-					<source src={Video} type="video/mp4" />
+					<source src={require("./trailer.mp4")} type="video/mp4" />
 					Your browser does not support HTML5 video.
 				</video>
 				<div className="fixed w-4/12 left-32 bottom-20">
 					<img
 						src="https://1000logos.net/wp-content/uploads/2021/04/Stranger-Things-logo.png"
 						className="w-60"
+						alt="Stranger Things"
 					/>
 					<div className=" text-xl">
 						When a young boy vanishes, a small town uncovers a mystery involving
